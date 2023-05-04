63c2b9011e061df6f8ed333249058319bcdde908
Steven Arevalo <sjarevalo.ext@ayesa.com>
2023-05-04 18:12:56 -0500

    Added tags feature

--- PruebasKraken/features/my_first.feature
+++ PruebasKraken/features/my_first.feature
@@ -1,11 +1,16 @@
-Feature: My feature
+Feature: Tags
 
-@user1 @mobile
-Scenario: My scenario 1
-  Given I wait
-  When I send a signal to user 2 containing "hi"
-
-@user2 @web
-Scenario: My scenario 2
-  Given I wait for a signal containing "hi"
-  When I wait
+@user1 @web
+Scenario: Como usuario quiero loguearme en la pagina, listar etiquetas y crear una etiqueta
+  Given I navigate to page "http://localhost:2368/ghost/#/signin"
+  And I wait for 3 seconds
+  When I enter email "myjachis@gmail.com"
+  And I enter password "Mr.hellno19"
+  And I wait for 2 seconds
+  And I click login button
+  And I wait for 3 seconds 
+  And I click tags button
+  And I wait for 5 seconds 
+  And I click New Tags button
+  And I enter the name input field "test"
+  And I wait for 5 seconds 
--- PruebasKraken/features/web/step_definitions/step.js
+++ PruebasKraken/features/web/step_definitions/step.js
@@ -1,1 +1,36 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+When("I enter email {string}", async function (email) {
+  let element = await this.driver.$("#ember8");
+  return await element.setValue(email);
+});
+
+When("I enter password {string}", async function (password) {
+  let element = await this.driver.$("#ember10");
+  return await element.setValue(password);
+});
+
+When("I click login button", async function () {
+  let element = await this.driver.$("#ember12");
+  return await element.click();
+});
+
+Then("I click tags button", async function () {
+  let element = await this.driver.$("#ember38");
+  return await element.click();
+});
+
+Then("I click New Tags button", async function () {
+  let element = await this.driver.$("a.gh-btn.gh-btn-green");
+  return await element.click();
+});
+
+Then("I enter the name input field {string}", async function (name) {
+  let element = await this.driver.$("input#tag-name");
+  return await element.setValue(name);
+});
+
+Then("I send the message", async function () {
+  let element = await this.driver.$("span.x3nfvp2:nth-child(3)");
+  return await element.click();
+});

361b365003b5d1e2f488d84140a7a04978d31351
Steven Arevalo <sjarevalo.ext@ayesa.com>
2023-05-05 15:45:26 -0500

    Added staff feature

--- PruebasKraken/features/web/step_definitions/step.js
+++ PruebasKraken/features/web/step_definitions/step.js
@@ -31,8 +31,8 @@ When("I enter the name input field {string}", async function (value) {
 });
 
 When("I enter the slug input field {string}", async function (value) {
-    let element = await this.driver.$("input#tag-slug");
-    return await element.setValue(value);
+  let element = await this.driver.$("input#tag-slug");
+  return await element.setValue(value);
 });
 
 When("I enter the color input field {string}", async function (value) {
@@ -115,5 +115,95 @@ When("I click in the publish button", async function () {
 
 When("I click post button", async function () {
   let element = await this.driver.$("a.blue.link.fw4");
+  return await element.click();
+});
+
+// *********************** STAFF FEATURE *********************************
+When("I click staff button", async function () {
+  let element = await this.driver.$(`a[href="#/staff/"].ember-view`);
+  return await element.click();
+});
+
+When("I click staff ghost button", async function () {
+  let element = await this.driver.$(`a[href="#/staff/ghost/"].ember-view`);
+  return await element.click();
+});
+
+When("I enter the user name input field {string}", async function (value) {
+  let element = await this.driver.$("input#user-name");
+  return await element.setValue(value);
+});
+
+When("I enter the email input field {string}", async function (value) {
+  let element = await this.driver.$("input#user-email");
+  return await element.setValue(value);
+});
+
+When("I enter the user location input field {string}", async function (value) {
+  let element = await this.driver.$("input#user-location");
+  return await element.setValue(value);
+});
+
+When("I enter the user new password input field {string}", async function (value) {
+  let element = await this.driver.$("input#user-password-new");
+  return await element.setValue(value);
+});
+
+When("I enter the user password verification input field {string}", async function (value) {
+  let element = await this.driver.$("input#user-new-password-verification");
+  return await element.setValue(value);
+});
+
+When("I click save change password button", async function () {
+  let element = await this.driver.$("button.gh-btn-red");
+  return await element.click();
+});
+
+When("I change the user role to Editor", async function () {
+  let element = await this.driver.$("select#new-user-role");
+  return await element.selectByIndex(1);
+});
+
+When("I click save edit staff button", async function () {
+  let element = await this.driver.$("button.gh-btn.gh-btn-blue.gh-btn-icon.ember-view");
+  return await element.click();
+});
+
+When("I click the engine button", async function () {
+  let element = await this.driver.$("button.gh-btn-white");
+  return await element.click();
+});
+When("I click the suspend button", async function () {
+  let element = await this.driver.$("button.suspend");
+  return await element.click();
+});
+
+When("I click the unsuspend button", async function () {
+  let element = await this.driver.$("button.unsuspend");
+  return await element.click();
+});
+
+When("I click the confirm suspend button", async function () {
+  let element = await this.driver.$("div.modal-footer button.gh-btn.gh-btn-red");
+  return await element.click();
+});
+
+When("I click the invite people button", async function () {
+  let element = await this.driver.$("button.gh-btn.gh-btn-green");
+  return await element.click();
+});
+
+When("I select the user role to Contributor", async function () {
+  let element = await this.driver.$("select#new-user-role");
+  return await element.selectByIndex(3);
+});
+
+When("I enter in the invite form email {string}", async function (email) {
+  let element = await this.driver.$(`input.email.ember-text-field.gh-input[type="email"]`);
+  return await element.setValue(email);
+});
+
+When("I click the send invitation button", async function () {
+  let element = await this.driver.$("div.modal-footer button.gh-btn.gh-btn-green");
   return await element.click();
 });

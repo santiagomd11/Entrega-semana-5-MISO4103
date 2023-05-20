b4af161dddf33f6ce0f0c36526d763488ade8620
camilo-cortez <c.cortez@uniandes.edu.co>
2023-05-20 14:18:45 -0500

    Add a priori (strategy 1) playwright tests

--- EntregaSemana7/PruebasPlaywright/PruebasPlayWrightEstrategia1/index.js
+++ EntregaSemana7/PruebasPlaywright/PruebasPlayWrightEstrategia1/index.js
@@ -4,6 +4,8 @@ const expect = require('@playwright/test');
 
 const url = 'http://localhost:2368/ghost';
 
+const mockData = require('./MockData.json');
+
 (async () => {
   
   //Definir los navegadores en los que se quiere hacer la prueba
@@ -80,31 +82,32 @@ async function logout(page, screenshotPath){
   await page.screenshot({path:`${screenshotPath}logoutscreen.png`});
 }
 
+function getRandomItem(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
 async function testScenario1(page){
-  //Scenario 1: Como usuario quiero iniciar sesion en la pagina, ver el listado de usuarios y ver los detalles de un usuario
+  //Scenario 1: Intentar iniciar sesion en la pagina, correo y pw aleatorios, assert
   //#region GIVEN
+  //await page.goto(url); hecho en la linea 24
   var screenshotPath = './imagenes-test/users-scenario1';
-  await login(page, screenshotPath);
   //#endregion
 
   //#region WHEN
-  //Go to users (staff)
-  await page.click('a[href="#/staff/"]')
-  await new Promise(r => setTimeout(r, 2000));
-  await page.screenshot({path:`${screenshotPath}-1-staffpage.png`})
-
-  //Go to user ghost
-  await page.locator('css=h3.apps-card-app-title').filter({ hasText: 'Ghost' }).click()
-  await new Promise(r => setTimeout(r, 2000));
-  await page.screenshot({path:`${screenshotPath}-2-ghostuser.png`})
-  console.log('Clicked on ghost user')
+  var item = getRandomItem(mockData);
+  await page.type('css=.email.ember-text-field.gh-input.ember-view', item.email);
+  await page.type('css=.password.ember-text-field.gh-input.ember-view', item.password);
+  await page.click('css=.login.gh-btn.gh-btn-blue')
+  await new Promise(r => setTimeout(r, 7000));
+  console.log(`Clicked on login button, URL is now ${page.url()}`)
+  await page.screenshot({path:`${screenshotPath}0-loginbutton.png`});
   //#endregion 
 
   //#region THEN
-  await expect.expect(page.locator('input#user-name')).toHaveValue('Ghost')
+  await expect.expect(page.getByText('There is no user with that email address.')).toBeVisible();
+  console.log('Expect ok');
   //#endregion
 
-  await logout(page, screenshotPath);
 }
 
 async function testScenario2(page){
@@ -127,7 +130,8 @@ async function testScenario2(page){
   console.log('Clicked on create new user')
 
   //Fill new user email new-user-email
-  var newEmail = 'kmilo2106@gmail.com'
+  var item = getRandomItem(mockData);
+  var newEmail = item.email;
   await page.type('css=#new-user-email', newEmail);
   await page.screenshot({path:`${screenshotPath}-3-fillnewUserEmail.png`})
   await page.getByRole('button', { name: 'Send invitation now', exact: true}).click();
@@ -166,6 +170,7 @@ async function testScenario3(page){
 
   //#region WHEN
   //Go to users (staff)
+  var item = getRandomItem(mockData);
   await page.click('a[href="#/staff/"]')
   await new Promise(r => setTimeout(r, 2000));
   await page.screenshot({path:`${screenshotPath}-1-staffpage.png`})
@@ -178,11 +183,11 @@ async function testScenario3(page){
 
   //Edit fields
   var editedUserName = 'Ghost_edited_user'
-  await page.fill('css=#user-email', 'ghost-author_edited@example.com');
+  await page.fill('css=#user-email', item.email);
   await page.locator('select#new-user-role').selectOption({ label: 'Contributor' })
-  await page.fill('css=#user-location', 'The Internet_edited');
-  await page.fill('css=#user-website', 'https://ghost-edited.org');
-  await page.fill('css=#user-bio', 'You can delete this user to remove all the welcome posts _edited');
+  await page.fill('css=#user-location', item.country);
+  await page.fill('css=#user-website', item.website);
+  await page.fill('css=#user-bio', item.words);
   await page.screenshot({path:`${screenshotPath}-3-editedUserFields.png`})
   console.log('Edited user fields')
   await new Promise(r => setTimeout(r, 2000));
@@ -195,8 +200,8 @@ async function testScenario3(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.locator('input#user-website')).toHaveValue('https://ghost-edited.org')
-  await expect.expect(page.locator('input#user-email')).toHaveValue('ghost-author_edited@example.com')
+  await expect.expect(page.locator('input#user-website')).toHaveValue(item.website)
+  await expect.expect(page.locator('input#user-email')).toHaveValue(item.email)
   console.log("Expect ok")
   //#endregion
 
@@ -224,6 +229,7 @@ async function testScenario4(page){
 
   //#region WHEN
   //Go to users (staff)
+  var item = getRandomItem(mockData);
   await page.click('a[href="#/staff/"]')
   await new Promise(r => setTimeout(r, 2000));
   await page.screenshot({path:`${screenshotPath}-1-staffpage.png`})
@@ -235,7 +241,7 @@ async function testScenario4(page){
   console.log('Clicked on create new user')
 
   //Fill new user email new-user-email
-  await page.type('css=#new-user-email', 'correo@gmail.com');
+  await page.type('css=#new-user-email', item.email);
   await page.screenshot({path:`${screenshotPath}-3-fillnewUserEmail.png`})
   await page.getByRole('button', { name: 'Send invitation now' }).click();
   console.log('Clicked on send invitation')
@@ -258,7 +264,7 @@ async function testScenario4(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.getByText('correo@gmail.com')).toBeHidden();
+  await expect.expect(page.getByText(item.email)).toBeHidden();
   console.log('Expect ok')
   //#endregion
 
@@ -286,10 +292,11 @@ async function testScenario5(page){
   console.log('Clicked on create new tag')
 
   //Fill new tag fields
-  await page.fill('input#tag-name', 'NewTag');
-  await page.fill('input#tag-slug', 'NewTag-Slug');
-  await page.getByPlaceholder("abcdef").fill("00ff00");
-  await page.fill('textarea#tag-description', 'lorem ipsum...');
+  var item = getRandomItem(mockData);
+  await page.fill('input#tag-name', item.companyName);
+  await page.fill('input#tag-slug', `${item.companyName}-slug`);
+  await page.getByPlaceholder("abcdef").fill(item.hexColor.replace('#',''));
+  await page.fill('textarea#tag-description', item.words);
   await new Promise(r => setTimeout(r, 1000));
   await page.screenshot({path:`${screenshotPath}-3-newTagFilled.png`})
   console.log('Filled new tag details')
@@ -302,7 +309,7 @@ async function testScenario5(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.getByRole('heading', { name: 'Tags NewTag' })).toBeVisible();
+  await expect.expect(page.getByRole('heading', { name: `Tags ${item.companyName}` })).toBeVisible();
   console.log('Expect ok')
   //#endregion
 
@@ -338,10 +345,11 @@ async function testScenario6(page){
   console.log('Clicked on create new tag')
 
   //Fill new tag fields
-  await page.fill('input#tag-name', 'NewTag');
-  await page.fill('input#tag-slug', 'NewTag-Slug');
-  await page.getByPlaceholder("abcdef").fill("00ff00");
-  await page.fill('textarea#tag-description', 'lorem ipsum...');
+  var item = getRandomItem(mockData);
+  await page.fill('input#tag-name', item.companyName);
+  await page.fill('input#tag-slug', `${item.companyName}-slug`);
+  await page.getByPlaceholder("abcdef").fill(item.hexColor.replace('#',''));
+  await page.fill('textarea#tag-description', item.words);
   await new Promise(r => setTimeout(r, 1000));
   await page.screenshot({path:`${screenshotPath}-3-newTagFilled.png`})
   console.log('Filled new tag details')
@@ -353,10 +361,11 @@ async function testScenario6(page){
   console.log('Saved new tag');
 
   //Edit new tag
-  await page.fill('input#tag-name', 'NewTag_edited');
-  await page.fill('input#tag-slug', 'NewTag-Slug_edited');
-  await page.getByPlaceholder("abcdef").fill("0000ff");
-  await page.fill('textarea#tag-description', 'lorem ipsum... edited');
+  var editItem = getRandomItem(mockData);
+  await page.fill('input#tag-name', editItem.companyName);
+  await page.fill('input#tag-slug', `${editItem.companyName}-slug`);
+  await page.getByPlaceholder("abcdef").fill(editItem.hexColor.replace('#',''));
+  await page.fill('textarea#tag-description', editItem.words);
   await new Promise(r => setTimeout(r, 1000));
   await page.screenshot({path:`${screenshotPath}-5-newTagEdited.png`})
   console.log('Edited new tag details')
@@ -369,7 +378,7 @@ async function testScenario6(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.locator('input#tag-name')).toHaveValue('NewTag_edited')
+  await expect.expect(page.locator('input#tag-name')).toHaveValue(editItem.companyName)
   console.log('Expect ok')
   //#endregion
 
@@ -405,10 +414,11 @@ async function testScenario7(page){
   console.log('Clicked on create new tag')
 
   //Fill new tag fields
-  await page.fill('input#tag-name', 'NewTag-Scen7');
-  await page.fill('input#tag-slug', 'NewTag-Slug');
-  await page.getByPlaceholder("abcdef").fill("00ff00");
-  await page.fill('textarea#tag-description', 'lorem ipsum...');
+  var item = getRandomItem(mockData);
+  await page.fill('input#tag-name', item.companyName);
+  await page.fill('input#tag-slug', `${item.companyName}-slug`);
+  await page.getByPlaceholder("abcdef").fill(item.hexColor.replace('#',''));
+  await page.fill('textarea#tag-description', item.words);
   await new Promise(r => setTimeout(r, 1000));
   await page.screenshot({path:`${screenshotPath}-3-newTagFilled.png`})
   console.log('Filled new tag details')
@@ -433,7 +443,7 @@ async function testScenario7(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.getByText('NewTag-Scen7')).toBeHidden();
+  await expect.expect(page.getByText(item.companyName)).toBeHidden();
   console.log('Expect ok')
   //#endregion
   
@@ -468,10 +478,11 @@ async function testScenario8(page){
   console.log('Clicked on create new internal tag')
 
   //Fill new tag fields
-  await page.fill('input#tag-name', '#NewInternalTag');
-  await page.fill('input#tag-slug', 'NewInternalTag-Slug');
-  await page.getByPlaceholder("abcdef").fill("ff00ff");
-  await page.fill('textarea#tag-description', 'lorem ipsum...');
+  var item = getRandomItem(mockData);
+  await page.fill('input#tag-name', `#${item.companyName}`);
+  await page.fill('input#tag-slug', `${item.companyName}-slug`);
+  await page.getByPlaceholder("abcdef").fill(item.hexColor.replace('#',''));
+  await page.fill('textarea#tag-description', item.words);
   await new Promise(r => setTimeout(r, 1000));
   await page.screenshot({path:`${screenshotPath}-4-newInternalTagFilled.png`})
   console.log('Filled new internal tag details')
@@ -484,7 +495,7 @@ async function testScenario8(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.getByRole('heading', { name: 'Tags #NewInternalTag' })).toBeVisible();
+  await expect.expect(page.getByRole('heading', { name: `Tags #${item.companyName}` })).toBeVisible();
   console.log('Expect ok')
   //#endregion
 
@@ -520,8 +531,9 @@ async function testScenario9(page){
   console.log('Clicked on create new post')
 
   //Fill new post fields
-  await page.getByPlaceholder("Post Title").fill("Test draft title");
-  await page.fill('css=.koenig-editor__editor.__mobiledoc-editor.__has-no-content', 'lorem ipsum...');
+  var item = getRandomItem(mockData)
+  await page.getByPlaceholder("Post Title").fill(item.companyName);
+  await page.fill('css=.koenig-editor__editor.__mobiledoc-editor.__has-no-content', item.words);
   await new Promise(r => setTimeout(r, 1000));
   await page.screenshot({path:`${screenshotPath}-3-newPostFilled.png`})
   console.log('Filled new post details')
@@ -533,12 +545,12 @@ async function testScenario9(page){
   //#endregion
 
   //#region THEN
-  await expect.expect(page.getByText('Test draft title')).toBeVisible();
+  await expect.expect(page.getByText(item.companyName)).toBeVisible();
   console.log('Expect ok')
   //#endregion
 
   //Open draft and delete it (cleanup)
-  await page.getByText('Test draft title').click()
+  await page.getByText(item.companyName).click()
   await new Promise(r => setTimeout(r, 500));
   await page.click('css=button.post-settings');
   await new Promise(r => setTimeout(r, 500));
@@ -572,7 +584,8 @@ async function testScenario10(page){
   console.log('Clicked on create new post')
 
   //Fill new post fields
-  var newPostTitle = 'Test draft title scen10'
+  var item = getRandomItem(mockData)
+  var newPostTitle = `${item.companyName} draft`
   await page.getByPlaceholder("Post Title").fill(newPostTitle);
   await page.fill('css=.koenig-editor__editor.__mobiledoc-editor.__has-no-content', 'lorem ipsum...');
   await new Promise(r => setTimeout(r, 1000));
